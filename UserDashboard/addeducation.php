5659d4d30a08c781fc84b0a800de6029ce91aaf9
Sanjanabekwadkar147 <Sanjanabekwadkar@gmail.com>
2024-05-09 10:27:17 +0530

    add new

--- UserDashboard/addeducation.php
+++ UserDashboard/addeducation.php
@@ -262,31 +262,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <i class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle"></i>
                     <h2 class="fs-2 m-0">Dashboard</h2>
                 </div>
-
-                <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button> -->
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <!-- <a href="Home.php" class="nav-link second-text fw-bold">
-                            <i class="fas fa-home me-2 fs-4"></i>  Adjust the font size here (e.g., fs-4) -->
-
-                        <!-- <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle second-text fw-bold" href="#" id="navbarDropdown"
-                                role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <i class="fas fa-user me-2"></i>John Doe
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Profile</a></li>
-                                <li><a class="dropdown-item" href="#">Settings</a></li>
-                                <li><a class="dropdown-item" href="#">Logout</a></li>
-                            </ul>
-                        </li> -->
-                    </ul>
-                </div>
             </nav>
             <div class="form-container">
                 <form action="" method="post" enctype="multipart/form-data">
